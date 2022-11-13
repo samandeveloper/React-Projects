2d6c0b0b2ab346aed9780ee422486ed3fc1b7e07
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2022-11-12 18:42:35 -0700

    Add files via upload

---
+++ tour-site/build/precache-manifest.b380d1e0963cd6219a5b204d63afab6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32eb6adf3c1d704ca2d64db6f72bf8f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b517c3a335e174736328",
+    "url": "/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b517c3a335e174736328",
+    "url": "/static/js/main.9524d041.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
