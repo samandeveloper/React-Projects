0e8bf53e8d83ee1a1af548d69e013df94750dbcc
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2022-11-12 18:56:52 -0700

    Update List.js

--- birthday-reminder/src/List.js
+++ birthday-reminder/src/List.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
-
 const List = (props) => {
   return (
-    //we can write the map here instead of App.js
     <>
         <div className='person'>
           <img src={`${props.image}`}/>
             <div>
               <h4>{props.name}</h4>
               <p>{props.age} years</p>
-              
             </div>
       </div>
     </>
