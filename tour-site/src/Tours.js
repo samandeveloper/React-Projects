f3db455de36f23ceab85bb13cd81dd2b1da72f5b
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2022-11-12 22:08:42 -0700

    Update Tours.js

--- tour-site/src/Tours.js
+++ tour-site/src/Tours.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import Tour from './Tour';
 
-const Tours = (props) => {
-
-const tourMap = props.tours.map(tour =>{
+const Tours = ({tours,removeTour}) => {
+const tourMap = tours.map((tour) =>{
   return(
-  <Tour removeTour={props.removeTour} key={tour.id} name={tour.name} info={tour.info} image ={tour.image} price={tour.price} />
+    <Tour removeTour={removeTour} key={tour.id} {...tour} />
   )
 })
-// console.log(removeTour)
   return(
     <section>
       <div className='title'>
         <h2>Our Tours</h2>  
         <div className='underline'></div>
       </div>
-
       <div>{tourMap}</div>
-      {/* <div> */}
-        {/* {tours.map((tour) =>{
-          return <Tour key={tour.id} name={tour.name} info={tour.info} image ={tour.image} price={tour.price} />
-        })} */}
-                {/* {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour}  />;
-        })} */}
-      {/* </div> */}
-
     </section>
-
   )
 };
 
