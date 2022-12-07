1323973d5704f1b9442b7a6107c1c7b117d297b4
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-07 12:35:47 -0700

    Updates

---
+++ precache-manifest.037b7c80801e50c0029baf7da1b8f5c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f04379e73b4c252b82934aba166ed8f",
+    "url": "/color-generator/index.html"
+  },
+  {
+    "revision": "cb995d5832ff1c347aee",
+    "url": "/color-generator/static/css/main.f6e8d1ab.chunk.css"
+  },
+  {
+    "revision": "0b728d42613a7c9ec6d3",
+    "url": "/color-generator/static/js/2.cdd6e70d.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color-generator/static/js/2.cdd6e70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb995d5832ff1c347aee",
+    "url": "/color-generator/static/js/main.10bfcac3.chunk.js"
+  },
+  {
+    "revision": "ed92ef5652ce8f57a79c",
+    "url": "/color-generator/static/js/runtime-main.c4c3dd28.js"
+  }
+]);
